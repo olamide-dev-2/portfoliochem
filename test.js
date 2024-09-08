3c07f5728e5da94c34e4f9695e15f152ea1d4930
olamide-dev-2 <tamiloresoleye@gmail.com>
2024-09-08 21:00:45 +0100

    Add files via upload

---
+++ test.js
@@ -0,0 +1,16 @@
+// Function to open the side menu
+function openMenu() {
+    document.getElementById("sideMenu").style.right = "0";
+}
+
+// Function to close the side menu
+function closeMenu() {
+    document.getElementById("sideMenu").style.right = "-16rem"; // Adjust this value based on the width of your menu
+}
+
+
+
+
+
+
+       
